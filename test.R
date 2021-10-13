284829f6f6e3828ca0fcbaa30fb393274b7f59d6
Erick <e.coronadoj@gmail.com>
2021-10-13 01:01:37 +0000

    Test. Primero push

---
+++ test.R
@@ -0,0 +1,14 @@
+### Esta es una prueba para hacer el push y pull
+
+autos  <- mtcars
+mean(mtcars$wt)
+
+
+usethis::create_github_token()
+
+install.packages("gitcreds")
+library(gitcreds)
+
+gitcreds_set()
+
+gitcreds_get()
